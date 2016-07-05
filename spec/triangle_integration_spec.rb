241d154c0e9fb4b71cd7eca6ae1b89bba6df4521
Caleb Stokka and Rony Nasr <caleb+rony@>
2016-07-05 09:26:51 -0700

    add additional integration specs

--- spec/triangle_integration_spec.rb
+++ spec/triangle_integration_spec.rb
@@ -12,6 +12,28 @@ describe('the triangle path', {:type => :feature}) do
     fill_in('side_2', :with => '4')
     fill_in('side_3', :with => '5')
     click_button('submit')
-    expect(page).to have_content('this is a triangle')
+    expect(page).to have_content('this is a scalene triangle')
+  end
+end
+
+describe('the triangle path', {:type => :feature}) do
+  it('gathers sides of a triangle and returns type of triangle') do
+    visit('/')
+    fill_in('side_1', :with => '4')
+    fill_in('side_2', :with => '4')
+    fill_in('side_3', :with => '4')
+    click_button('submit')
+    expect(page).to have_content('this is an equilateral triangle')
+  end
+end
+
+describe('the triangle path', {:type => :feature}) do
+  it('gathers sides of a triangle and returns type of triangle') do
+    visit('/')
+    fill_in('side_1', :with => '1')
+    fill_in('side_2', :with => '4')
+    fill_in('side_3', :with => '9')
+    click_button('submit')
+    expect(page).to have_content('this is not a triangle')
   end
 end
